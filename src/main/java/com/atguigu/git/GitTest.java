2972e91fcd762046948d88d954b78cd88e66ad6a
james-jordan <245093169@qq.com>
2022-03-21 12:04:34 +0800

    第三次提交（第二次修改版本）

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello IDEA Git");
         System.out.println("第二次修改");
+        System.out.println("hello git3!");
     }
 }
